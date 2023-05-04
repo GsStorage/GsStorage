6ec5b526e7acff6a784c35e201b1b3123d5c75a0
GsStorage <1753368222@qq.com>
2023-05-04 15:02:13 +0800

    添加项目文件。

---
+++ 1,2,3,4组成不重复的三位数/1,2,3,4组成不重复的三位数.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int a, b, c;
+	int i = 0;
+	for (a = 1; a < 5; a++) 
+	{
+		for (b = 1; b < 5; b++)
+		{
+			for (c = 1; c < 5; c++)
+			{
+				if (a != b && b != c && a != c) 
+				{
+					printf("%d%d%d ", a, b, c);
+				}
+			}
+		}
+	}
+}
